8959a6327c72e9790d621c5234a0a6b7cc0efe10
Anton Zvyagintsev <antonzvyagi@gmail.com>
2023-05-03 19:14:13 +0400

    order client

--- pkg/order/client.go
+++ pkg/order/client.go
@@ -1,1 +1,24 @@
 package order
+
+import (
+	"fmt"
+
+	"github.com/antonzvyagi/grpc-api-gateway/pkg/config"
+	"github.com/antonzvyagi/grpc-api-gateway/pkg/order/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type ServiceClient struct {
+	Client pb.OrderServiceClient
+}
+
+func InitServiceClient(c *config.Config) pb.OrderServiceClient {
+	cc, err := grpc.Dial(c.OrderSvcUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		fmt.Println("Could not connect:", err)
+	}
+
+	return pb.NewOrderServiceClient(cc)
+}
